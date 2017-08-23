68ef39d1cdcbb3a4d82f3645930606100a737163
Ronaldo Caldas da Silva <ronaldocaldas@gmail.com>
2017-08-23 14:06:11 -0300

    commit inicial de criação do projeto

---
+++ src/main/java/br/com/ronaldo/robot/NasaRobotExplorerApplication.java
@@ -0,0 +1,12 @@
+package br.com.ronaldo.robot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NasaRobotExplorerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NasaRobotExplorerApplication.class, args);
+	}
+}
---
+++ src/test/java/br/com/ronaldo/robot/NasaRobotExplorerApplicationTests.java
@@ -0,0 +1,16 @@
+package br.com.ronaldo.robot;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class NasaRobotExplorerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
