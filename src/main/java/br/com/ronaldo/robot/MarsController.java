b0669587959d5e975a8d3d75cd76c18195c2d6c0
Ronaldo Caldas da Silva <ronaldocaldas@gmail.com>
2017-08-23 21:32:15 -0300

    Método POST funcionando com parâmentro enviado via URL http://localhost:8080/rest/mars/MMRMMRMM

--- src/main/java/br/com/ronaldo/robot/MarsController.java
+++ src/main/java/br/com/ronaldo/robot/MarsController.java
@@ -3,22 +3,16 @@ package br.com.ronaldo.robot;
 import java.util.Map;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class MarsController {
 
-	@RequestMapping("/")
-	public String welcome(Map<String, Object> model) {
-		model.put("message", "Olá mundo");
-		return "welcome";
-	}
-
-	@GetMapping
-	public ModelAndView listar() {
-		ModelAndView modelAndView = new ModelAndView("mars");
-		return modelAndView;
+	@RequestMapping(value="/rest/mars/{movimento}",method = RequestMethod.POST)
+	public String welcome(@PathVariable("movimento") String movimento, Map<String, Object> model) {
+		model.put("movimento", movimento);
+		return "mars";
 	}
 }
