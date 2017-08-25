907a61c8420ce6414490974956d32a60306f8e81
Ronaldo Caldas da Silva <ronaldocaldas@gmail.com>
2017-08-25 15:43:37 -0300

    Ultima modificação do projeto com limpando as dependências não necessárias (devtools) e thymeleaf e tambem a exclusão do método  GET e as tamplates utilizadas para teste.

--- src/main/java/br/com/ronaldo/robot/controller/MarsController.java
+++ src/main/java/br/com/ronaldo/robot/controller/MarsController.java
@@ -6,12 +6,10 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.ModelAndView;
 
 import br.com.ronaldo.robot.model.Position;
 import br.com.ronaldo.robot.model.Robot;
@@ -27,12 +25,6 @@ public class MarsController {
 		robo = new Robot(); 
 		robo.processEntry(movimento);
 		 return robo.getPosicao();
-	}
-	
-	@GetMapping
-	public ModelAndView telaWelcome() {
-		ModelAndView modelAndView = new ModelAndView("welcome");
-		return modelAndView;
 	}
 
 	@ExceptionHandler
